1009406d9da9f97df02a04a60e531441a4f413f4
glinka <alexandraglinka@yandex.ru>
2023-03-24 21:36:18 +0300

    isAdult

---
+++ src/lesson13_classes_methods/test1/Main.java
@@ -0,0 +1,19 @@
+package lesson13_classes_methods.test1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Person sasha = new Person("Саша", 26);
+//        if (sasha.getAge() >= 18) {                   ////// убираем в метод !
+//            System.out.println("Иди в бар");
+//        } else {
+//            System.out.println("Иди в школу");
+//        }
+        if (sasha.isAdult()) {
+            System.out.println("Иди в бар");
+        } else {
+            System.out.println("Иди в школу");
+        }
+    }
+}
---
+++ src/lesson13_classes_methods/test1/Person.java
@@ -0,0 +1,27 @@
+package lesson13_classes_methods.test1;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isAdult() {
+        return this.getAge() > 18;
+    }
+}
